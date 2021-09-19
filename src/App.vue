43828ccb132097e0c9ce16bc15e31639febf9a0a
josephracca <josephracca@gmail.com>
2021-09-19 02:42:50 -0700

    used backend to implement CRUD functions

--- src/App.vue
+++ src/App.vue
@@ -10,35 +10,27 @@
         title="Task Tracker"
         v-bind:showAddTask="showAddTask"
       />
-      <div v-show="showAddTask">
-        <AddTask @add-task="addTask" />
-      </div>
-      <Tasks
-        @toggle-reminder="toggleReminder"
-        @delete-task="deleteTask"
-        v-bind:tasks="tasks"
-      />
+
     </header>
+    <router-view v-bind:show-add-task="showAddTask" ></router-view>
+  <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
-import Tasks from "./components/Tasks";
-import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
 
 export default {
   name: "App",
   components: {
     // when you want to use a component you ahve to declare it here in the components object, then you can pass it into the template above, which takes a message prop
     Header,
-    Tasks,
-    AddTask,
+    Footer,
   },
   data() {
     return {
       showAddTask: false,
-      tasks: [],
       //data is going to be a function that returns an object and we're going top have tasks,
       //NOTE: we could put the tasks in here but we don't want to do that because we want to use a LIFECYCLE METHOD
       //when a component is loaded it goes through a specific lifecylce where there are certain methods/functions that are fired off that you can hook into to do specific things at specific times
@@ -46,88 +38,9 @@ export default {
     };
   },
   methods: {
-    async addTask(task) {
-      // console.log(task);
-      const res = await fetch("api/tasks", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(task),
-      });
-
-      const data = await res.json();
-
-      // console.log(data);
-
-      this.tasks = [...this.tasks, data];
-    },
-    async deleteTask(id) {
-      if (confirm("Are you sure?")) {
-        const res = await fetch(`api/tasks/${id}`, {
-          method: "DELETE",
-        });
-
-        res.status === 200
-          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
-          : alert("Error deleting task");
-      }
-    },
-    async toggleReminder(id) {
-      const taskToToggle = await this.fetchTask(id);
-      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
-      const res = await fetch(`api/tasks/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(updTask),
-      });
-
-      const data = await res.json();
-
-      // this.tasks[id].reminder = !this.tasks[id].reminder;
-      this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: data.reminder } : task
-      );
-    },
     toggleAddTask() {
       this.showAddTask = !this.showAddTask;
     },
-    async fetchTasks() {
-      const res = await fetch("api/tasks");
-      const data = await res.json();
-      return data;
-    },
-    async fetchTask(id) {
-      const res = await fetch(`api/tasks/${id}`);
-      const data = await res.json();
-      return data;
-    },
-  },
-  async created() {
-    this.tasks = await this.fetchTasks();
-    // [
-    //   {
-    //     id: 1,
-    //     text: 'Doctors Appointment',
-    //     day: 'October 1st at 5:30pm',
-    //     reminder: true
-    //   },
-    //   {
-    //     id: 2,
-    //     text: 'Hair Appointment',
-    //     day: 'October 4th at 5:30pm',
-    //     reminder: true
-    //   },
-    //   {
-    //     id: 3,
-    //     text: 'Gym Time',
-    //     day: 'November 1st at 5:30pm',
-    //     reminder: false
-    //   },
-
-    // ]
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import { createApp } from 'vue'
 //also importing the root app component
 import App from './App.vue'
 
+//pulling in the router file
+import router from './router'
+
 //passing in the main app component and mounting it to the div with the id of app
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
